22fedd169a10fe7a30fd8e653a03f5125c792b4f
jhen0605 <amy95173@gmail.com>
2021-03-14 01:58:22 +0800

    Initial Commit

---
+++ sketch-zeplin2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sketch-zeplin2
+//
+//  Created by 簡吟真 on 2021/3/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
